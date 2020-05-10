e4d384587210d7ea73d6535142c721c350020387
Bartosz <burtekdotryw@gmail.com>
2020-05-11 00:22:02 +0200

    Fix fb-untract-links.js META

--- fb-untract-links.js
+++ fb-untract-links.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Facebook untrack links
 // @namespace    https://github.com/burtek/tampermonkey-scripts
-// @version      1.0
-// @description  Remove facebook tracking data from any link you open or copy through facebook.\n\nIf something doesn't work let me know, I'll fix it!
-// @author       burtek
+// @version      1.0.1
+// @description  Remove facebook tracking data from any link you open or copy through facebook. If something doesn't work let me know and I'll fix it!
+// @author       burtek <burtekdotryw@gmail.com>
 // @match        https://www.facebook.com/*
 // @run-at       document-body
+// @supportURL   https://github.com/burtek/tampermonkey-scripts/issues
 // ==/UserScript==
 
 (function(TRACKING_KEYS) {
